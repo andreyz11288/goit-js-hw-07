20a437eae8fb091b4770056b8eca346d478d094e
andreyz11288 <andreyz11288@gmail.com>
2020-09-30 19:48:10 +0300

    1task ok

--- task-1.js
+++ task-1.js
@@ -7,14 +7,10 @@
 // Категория: Животные
 // Количество элементов: 4
 
-const categoriesList = Array.from(
-  document.querySelectorAll("ul#categories li.item ul ")
-);
-console.log(`В списке ${categoriesList.length} категории.`);
-
 const items = document.querySelectorAll("#categories .item");
+console.log(`В списке ${items.length} категории.`);
 Array.prototype.forEach.call(items, (element) => {
   const title = element.querySelector("h2").innerHTML;
   const itemsLength = element.querySelectorAll("li").length;
-  console.log(`Категория: ${title} \n Количество элементов: ${itemsLength}`);
+  console.log(`Категория: ${title} \nКоличество элементов: ${itemsLength}`);
 });
