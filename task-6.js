a032c297c8261dde9ce12e8ef967880fb6ef91f5
andreyz11288 <andreyz11288@gmail.com>
2020-10-02 16:41:34 +0300

    task 6

--- task-6.js
+++ task-6.js
@@ -1,1 +1,15 @@
+const inputEl = document.querySelector("#validation-input");
+const lengthEl = inputEl.getAttribute("data-length");
+console.log(inputEl);
+console.log(inputEl.value.length);
 
+inputEl.addEventListener("blur", eventList);
+function eventList() {
+  if (inputEl.value.length >= lengthEl) {
+    inputEl.setAttribute("class", "valid");
+    console.log("ok");
+  } else {
+    inputEl.setAttribute("class", "invalid");
+    console.log("fake");
+  }
+}
